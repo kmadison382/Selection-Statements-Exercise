4e6cd537bce0a5880f6f741f4e2ee60cffd1c9ea
kmadison382 <k.madison.382@gmail.com>
2024-06-11 21:43:28 -0500

    wrote a number game

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -2,9 +2,37 @@
 {
     public class Program
     {
+        public static void NumberGame(int favNumber)
+        {
+
+            Console.WriteLine("I'm thinking of a number between 1 and 10...");
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("That's too low!");
+                NumberGame(favNumber);
+            }
+
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("That's too high!");
+                NumberGame(favNumber);
+
+            }
+
+            else
+            {
+                Console.WriteLine("That's not ri---Oh, you guessed it! Good one!");
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            int favNumber = r.Next(1, 1000);
+
+            NumberGame(favNumber);
         }
     }
 }
